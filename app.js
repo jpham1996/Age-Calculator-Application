dedc0d2064ad34487e84100dd1cabbfe80913d1f
Jamie Pham <jamiepham14@gmail.com>
2023-12-09 09:39:39 -0500

    Made age inputs along vertically and added form validation errors

--- app.js
+++ app.js
@@ -2,77 +2,86 @@ const form = document.querySelector("#age__form");
 const monthInput = document.querySelector("#month");
 const dayInput = document.querySelector("#day");
 const yearInput = document.querySelector("#year");
-const ageResult = document.querySelector(".age__result");
-const error = document.querySelector(".error");
-const errorMessage = new Array();
+let ageResult = document.querySelector(".age__result");
 
 const today = new Date();
-const currentDay = today.getDate();
-const currentMonth = today.getMonth() + 1;
-const currentYear = today.getFullYear();
+let currentDay = today.getDate();
+let currentMonth = today.getMonth() + 1;
+let currentYear = today.getFullYear();
 
-const numberRegex = /^[0-9]*$/;
 const daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 // January, Feburary, March, April, May, June
-// July, August, September, October, November, December
+// July, August, September, October, November,   December
 
 function errorValidations() {
-  let messageHTML = "";
-
-  if (!monthInput.value.match(numberRegex)) {
-    errorMessage.push("test1");
+  if (isNaN(monthInput.value)) {
+    document.querySelector(".errorMessageMonth").innerText =
+      "Please enter a number";
   } else if (
     monthInput.value === "" ||
     !(monthInput.value >= 1 && monthInput.value <= 12)
   ) {
-    errorMessage.push("Please enter a month value between 1 and 12");
+    document.querySelector(".errorMessageMonth").innerText =
+      "Please enter a month value between 1 and 12";
+  } else {
+    document.querySelector(".errorMessageMonth").innerText = "";
   }
 
-  if (!dayInput.value.match(numberRegex)) {
-    errorMessage.push("test2");
+  if (isNaN(dayInput.value)) {
+    document.querySelector(".errorMessageDay").innerText =
+      "Please enter a number";
   } else if (
     dayInput.value === "" ||
     !(dayInput.value >= 1 && dayInput.value <= 31)
   ) {
-    errorMessage.push("Please enter a day value between 1 and 31");
+    document.querySelector(".errorMessageDay").innerText =
+      "Please enter a day value between 1 and 31";
+  } else {
+    document.querySelector(".errorMessageDay").innerText = "";
   }
 
-  if (!yearInput.value.match(numberRegex)) {
-    errorMessage.push("test3");
+  if (isNaN(yearInput.value)) {
+    document.querySelector(".errorMessageYear").innerText =
+      "Please enter a number";
   } else if (
     yearInput.value === "" ||
     !(yearInput.value >= 1000 && yearInput.value <= currentYear)
   ) {
-    errorMessage.push("Please enter a year value between 1000 and 2023");
+    document.querySelector(".errorMessageYear").innerText =
+      "Please enter a year value between 1000 and 2023";
+  } else {
+    document.querySelector(".errorMessageYear").innerText = "";
   }
-
-  errorMessage.forEach(function (message) {
-    messageHTML += "<li>" + message + "</li>";
-  });
-
-  error.innerHTML = messageHTML;
 }
 
 function calculateAge() {
-  if (dayInput.value > currentDay) {
-    currentDay += daysPerMonth[currentMonth - 1];
-    currentMonth -= 1;
-  }
+  // if (dayInput.value > currentDay) {
+  //   currentDay += daysPerMonth[currentMonth - 1];
+  //   currentMonth -= 1;
+  // }
 
-  if (monthInput.value > currentMonth) {
-    currentMonth += 12;
-    currentYear -= 1;
-  }
+  // if (monthInput.value > currentMonth) {
+  //   currentMonth += 12;
+  //   currentYear -= 1;
+  // }
 
-  const ageYear = currentYear - yearInput.value;
-  const ageMonth = currentMonth - monthInput.value;
   const ageDay = currentDay - dayInput.value;
+  const ageMonth = currentMonth - monthInput.value;
+  const ageYear = currentYear - yearInput.value;
 
-  ageResult.innerText = `You Age is ${ageYear} years, ${ageMonth} months, and ${ageDay} days.`;
+  if (isNaN(ageYear) || isNaN(ageMonth) || isNaN(ageDay)) {
+    ageResult.innerText = "";
+  } else {
+    ageResult.innerText = `You Age is ${ageYear} years, ${ageMonth} months, and ${ageDay} days.`;
+  }
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
   errorValidations();
   calculateAge();
 });
+
+// Leap Year
+// return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
